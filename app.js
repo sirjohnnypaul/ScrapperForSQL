53161da8df1399bb68af1e421feec23f5dc0f01a
Jan Szczęśniewicz <janszczesniewicz@icloud.com>
2020-06-04 01:22:58 +0200

    closed

--- app.js
+++ app.js
@@ -33,25 +33,9 @@ app.use(cors());
 app.use('/', express.static("public"))
 app.use('/', all);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next)
-// {
-// 	let err = new Error('Page not found!');
-// 	err.status = 404;
-
-// 	next(err);
-// });
-
-// //error handler
-// app.use(function(err, req, res, next)
-// {
-// 	// set locals, only providing error in development
-// 	res.locals.message = err.message;
-// 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-// 	// render the error page
-//     res.status(err.status || 500).json({ errors : [ { field: "general", error: err.message } ]})
-// });
+app.use('/*',(req,res,next) => {
+  res.json("404 - Not found ;)")
+})
 
 
 //start 
